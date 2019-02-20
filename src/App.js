eac83a0387ef18a85ae5ced77cccd0d93fe90cf6
Suleiman Ahmed <sule23ng@hotmail.co.uk>
2019-02-20 22:10:44 +0000

    app components created..

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Title from './panels/Title';
+import Form from './panels/Form';
+import Weather from './panels/Weather';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+//api key generated from https://home.openweathermap.org/api_keys
+const Api_Key = "485c4a0747189164e23a829b7f4924c8";
+
+class App extends React.Component{
+
+state = {
+  temperature: "",
+  city: "",
+  country: "",
+  humidity: "",
+  description: "",
+  error: ""
 }
 
+getWeather = async (e) => {
+  e.preventDefault();
+
+  const city = e.target.elements.city.value;
+  const country = e.target.elements.country.value;
+  
+  if(city === "" || country === "")
+  {
+    this.setState({
+      error: "Please, complete both fields"
+    })
+    return;
+  }
+  //For Celsius, we use units = metric
+  //For Fahrenheit, we use units = imperial
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${Api_Key}`);
+  
+  const response = await api_call.json();
+  
+  this.setState({
+    //the temperature should be a whole number.
+    //"\xB0 Celsius" is for the Celsius symbol.
+    temperature: Math.round(response.main.temp) + "\xB0 Celsius",
+    city: response.name,
+    country: response.sys.country,
+    humidity: response.main.humidity,
+    description: response.weather[0].description,
+    error: ""
+  })
+  
+}
+   render(){
+    return(
+      <div>
+      <div className="wrapper">
+        <div className="main">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-5 title-container">
+              <Title />
+            </div>
+            <div className="col-xs-7 form-container">
+              <Form loadWeather={this.getWeather} />
+              <Weather
+                temperature={this.state.temperature}
+                city={this.state.city}
+                country={this.state.country}
+                humidity={this.state.humidity}
+                description={this.state.description}
+                error={this.state.error}
+              />
+            </div>
+          </div>
+          </div>
+        </div>
+        </div>
+      </div>
+   )
+  }
+}
 export default App;
---
+++ src/panels/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Titles = (props) =>{
+  return (
+    <div>
+      <h1 className="title-container__title">Suleiman's Weather App </h1>
+      <p className="title-container__subtitle"> Check the weather of your location </p>
+    </div>
+  )
+}
+export default Titles;
